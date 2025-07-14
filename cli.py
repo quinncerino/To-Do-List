19ba91d27555c697871be7806e545251dbee18eb
quinncerino <quinncerino@gmail.com>
2025-07-14 14:53:06 -0400

    finished add and edit buttons

--- cli.py
+++ cli.py
@@ -7,7 +7,7 @@ now = time.strftime("%b %d, %Y %H:%M:%S")
 print("It is", now)
 
 while True:
-    user_action = input("Type add, show, complete or exit: ")
+    user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
 
     if user_action.startswith("add"):
